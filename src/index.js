787406f8cfa0b7cdc532eed1f0f250da185fcd69
yortega216 <yortega216@gmail.com>
2025-02-26 19:16:02 -0600

    Injected timestamp and icon condition

--- src/index.js
+++ src/index.js
@@ -47,6 +47,13 @@ function handleSearchSubmit(event) {
    
     searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+    let date = new Date(timestamp *1000);
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
+
 function getForecast(city) {
     let apiKey = "0df88fc3bb43t3261o93a95825416cbf";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
@@ -55,25 +62,26 @@ function getForecast(city) {
  }
 
 function displayForecast(response) {
-console.log(response.data);
-
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+   
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
     forecastHtml =
       forecastHtml +
       `<div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">⛅︎</div>
-  <div class="weather-forecast-temperatures">
-    <div class="weather-forecast-temperature">
-      <strong>15º</strong>
-    </div>
-    <div class="weather-forecast-temperature">9º</div>
-  </div>
-</div>
+       <div class="weather-forecast-date">${formatDay(day.time)}</div>
+       <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+       <div class="weather-forecast-temperatures">
+       <div class="weather-forecast-temperature">
+       <strong>${Math.round(day.temperature.maximum)}º</strong>
+       </div>
+       <div class="weather-forecast-temperature">${Math.round(
+         day.temperature.minimum)}º</div>
+       </div>
+       </div>
       `;
+       }
   });
 
   let forecastElement = document.querySelector("#forecast");
